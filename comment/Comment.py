5c25f8359f3204de2bd4e1c5069615d5e37fba1f
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2024-05-21 12:20:19 +0700

    add file

---
+++ comment/Comment.py
@@ -0,0 +1,97 @@
+import requests
+import logging
+import csv
+from requests import Response
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [ %(levelname)s ]\t:: %(message)s', datefmt="%Y-%m-%dT%H:%M:%S")
+
+class Comment:
+    def __init__(self) -> None:
+        self.__result: dict = {}
+        self.__result["caption"]: str = None
+        self.__result["date_now"]: str = None
+        self.__result["video_url"]: str = None
+        self.__result["comments"]: list = []
+
+    def __format_date(self, milisecond: int) -> str:
+        try:
+            return datetime.fromtimestamp(milisecond).strftime("%Y-%m-%dT%H:%M:%S")
+        except:
+            return datetime.fromtimestamp(milisecond / 1000).strftime("%Y-%m-%dT%H:%M:%S")
+
+    def __get_replies(self, commentid: str) -> list:
+        [data, i] = [[], 0]
+
+        while(True):
+            res: Response = requests.get(f'https://www.tiktok.com/api/comment/list/reply/?aid=1988&comment_id={commentid}&count=9999999&cursor={i * 50}').json()
+            
+            if(not res['comments']): break
+
+            data += res['comments']
+            i += 1
+
+        return self.__filter_comments(data)
+
+    def __filter_comments(self, comments: list) -> list:
+        new_comments: list = []
+
+        for comment in comments:
+            if(comment['share_info']['desc']): logging.info(comment['share_info']['desc'])
+
+            new_comment = {
+                "username": comment['user']['unique_id'],
+                "nickname": comment['user']['nickname'],
+                "comment": comment['text'],
+                'create_time': self.__format_date(comment['create_time']),
+                "avatar": comment['user']['avatar_thumb']['url_list'][0],
+                "total_reply": comment.get('reply_comment_total', 0),
+                "replies": self.__get_replies(comment['cid']) if comment.get('reply_comment_total', 0) > 0 else [] 
+            }
+
+            new_comments.append(new_comment)
+
+        return new_comments
+
+    def execute(self, videoid: str, size: int) -> None:
+        logging.info(f'Starting Scrapping for video with id {videoid}[{size} - {size + 50}]...')
+
+        res: Response = requests.get(f'https://www.tiktok.com/api/comment/list/?aid=1988&aweme_id={videoid}&count=9999999&cursor={size}').json()
+
+        if(res['status_code'] > 0): return logging.error('invalid id video')
+
+        try:
+            self.__result['caption'] = res['comments'][0]['share_info']['title']
+            self.__result['date_now'] = self.__format_date(res['extra']['now'])
+            self.__result['video_url'] = res['comments'][0]['share_info']['url']
+            self.__result['comments'] = self.__filter_comments(res['comments'])
+
+        except:
+            return logging.error('comments are over')
+
+        return self.__result
+
+    def to_csv(self, output_dir: str, videoid: str):
+        csv_file_path = f'{output_dir}/{videoid}.csv'
+        with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csv_file:
+            fieldnames = ['username', 'nickname', 'comment', 'create_time', 'avatar', 'total_reply', 'replies']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for comment in self.__result['comments']:
+                writer.writerow({
+                    'username': comment['username'],
+                    'nickname': comment['nickname'],
+                    'comment': comment['comment'],
+                    'create_time': comment['create_time'],
+                    'avatar': comment['avatar'],
+                    'total_reply': comment['total_reply'],
+                    'replies': comment['replies']
+                })
+
+        logging.info(f'Output CSV data : {csv_file_path}')
+
+# testing
+if __name__ == '__main__':
+    comment: Comment = Comment()
+    comment.execute('717013')
---
+++ main.py
@@ -0,0 +1,46 @@
+import re
+import os
+import requests
+import math
+
+from json import dumps
+from argparse import ArgumentParser
+from comment.Comment import Comment  # Adjusted import statement
+import logging  # Logging should be imported separately
+
+if __name__ == '__main__':
+    argp: ArgumentParser = ArgumentParser()
+    argp.add_argument("--url", '-u', type=str, default='7170139292767882522')
+    argp.add_argument("--size", '-s', type=int, default=50)
+    argp.add_argument("--output", '-o', type=str, default='data')
+    args = argp.parse_args()
+
+    if "vm.tiktok.com" in args.url or "vt.tiktok.com" in args.url:
+        videoid = requests.head(args.url, stream=True, allow_redirects=True, timeout=5).url.split("/")[5].split("?", 1)[0]
+    elif re.match("^\d+$", args.url):
+        videoid = args.url
+    else:
+        videoid = args.url.split("/")[5].split("?", 1)[0]
+    
+    comment: Comment = Comment()
+
+    json_full, dummy = [], {}
+
+    for i in range(math.ceil(args.size / 50)):
+        data: dict = comment.execute(videoid, i * 50)
+
+        output: str = f'{args.output}/{videoid}'
+
+        if not os.path.exists(output):
+            os.makedirs(output)
+
+        if data:
+            dummy = data
+            json_full += data['comments']
+
+            with open(f'{output}/{i * 50}-{(i + 1) * 50}.json', 'w') as file:
+                file.write(dumps(data, ensure_ascii=False, indent=2))
+                logging.info(f'Output data : {output}/{i * 50}-{(i + 1) * 50}.json')
+
+    # Save the data to a CSV file
+    comment.to_csv(output, videoid)
